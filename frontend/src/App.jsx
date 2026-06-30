c6b2bc48ebc974967f13b7392e2a2bc226baaea6
Hashim <hashimwisamsami@email.com>
2026-06-30 15:21:07 +0300

    create ProtectedRoute.jsx.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import ProtectedRoute from "./shared/ProtectedRoute";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      {/* Protected Routes */}
+      {/* Admin */}
+      <Route element={<ProtectedRoute allowedRole="admin" />}></Route>
     </Routes>
   );
 };
