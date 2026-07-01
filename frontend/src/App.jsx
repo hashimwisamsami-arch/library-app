261483fd3c388b1dafa32d842274336183c07a0c
Hashim <hashimwisamsami@email.com>
2026-07-01 10:17:12 +0300

    create admin dashboard page with LibraryContext and Admin layout with protected Routes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ProtectedRoute from "./shared/ProtectedRoute";
+import AdminLayout from "./admin/AdminLayout";
+import AdminDashboardPage from "./admin/AdminDashboardPage";
 const App = () => {
   return (
     <Routes>
@@ -11,7 +13,12 @@ const App = () => {
       <Route path="/signup" element={<Signup />} />
       {/* Protected Routes */}
       {/* Admin */}
-      <Route element={<ProtectedRoute allowedRole="admin" />}></Route>
+      <Route element={<ProtectedRoute allowedRole="admin" />}>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Navigate to="/admin/dashboard " replace />} />
+          <Route path="dashboard" element={<AdminDashboardPage />} />
+        </Route>
+      </Route>
     </Routes>
   );
 };
